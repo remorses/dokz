fe9250eeb8be186b55350c3fb83bc82cfbad9171
remorses <beats.by.morse@gmail.com>
2020-04-17 13:37:53 +0200

    dokz/src/components/Playground: default ifrme is false

--- dokz/src/components/Playground.tsx
+++ dokz/src/components/Playground.tsx
@@ -15,7 +15,7 @@ import { CopyButton } from './Code'
 const CLEAR_PADDING = `<style> body { padding: 0; margin: 0; width: 100%; height: auto !important; }  </style>`
 const INITIAL_IFRAME_CONTENT = `<!DOCTYPE html><html><head> ${CLEAR_PADDING} </head><body><div></div></body></html>`
 
-const IS_DEFAULT_IFRAME_ACTIVATED = true
+const IS_DEFAULT_IFRAME_ACTIVATED = false
 
 export const Playground = ({
     className,
