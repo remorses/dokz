17ce742c3f4c71a23b1a1ff9287c7dc1093f422a
remorses <beats.by.morse@gmail.com>
2020-06-26 18:36:42 +0200

    website/pages: added blog docs

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -26,7 +26,7 @@ const Page = () => (
                 logo={<Box as={Logo} width='30px' />}
                 navs={[
                     <a href='https://github.com/remorses/dokz'>Github</a>,
-                    <a href='/docs'>Demo</a>,
+                    // <a href='/docs'>Demo</a>,
                     <a href='/docs'>Docs</a>,
                     <a href='/blog'>Blog</a>,
                     <CtaButton px='10px' />,
