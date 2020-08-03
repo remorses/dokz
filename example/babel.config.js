0f68b7c106cb34820b9ba9a84310de624c773716
remorses <beats.by.morse@gmail.com>
2020-08-03 17:02:40 +0200

    example: edit this page

---
+++ example/babel.config.js
@@ -0,0 +1,14 @@
+// you need to give access to the github app fist, https://github.com/apps/edit-this-page-app
+module.exports = {
+    presets: ['next/babel'],
+    plugins: [
+        [
+            'edit-this-page',
+            {
+                editableFiles: 'pages/**',
+                githubUrl: 'https://github.com/remorses/dokz',
+                branch: 'master',
+            },
+        ],
+    ],
+}
